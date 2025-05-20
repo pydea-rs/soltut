cf4afde457bf8a88d76250578213bd13b45e9577
pya-h <p4ya.py@gmail.com>
2025-05-20 20:52:37 +0330

    complete initial impl of pred mark

--- dapp/nostradamus/anchor/programs/nostradamus/src/lib.rs
+++ dapp/nostradamus/anchor/programs/nostradamus/src/lib.rs
@@ -6,46 +6,81 @@ const DISCRIMINATOR_SIZE: usize = 8;
 
 declare_id!("FqzkXZdwYjurnUKetJCAvaUw5WAqbwzU6gZEwydeEfqS");
 
-
 #[program]
 pub mod nostradamus {
     use super::*;
+
+    pub fn initialize_markets_count(ctx: Context<InitializeMarketsCount>) -> Result<()>{
+        ctx.accounts.markets_count.value = 0;
+        Ok(())
+    }
 
     pub fn initialize_prediction_market(
         ctx: Context<InitializePredictionMarket>,
         question: String,
         start_at: u64,
         close_at: u64,
-        outcomes_count: u8,
     ) -> Result<()> {
         let pm = &mut ctx.accounts.prediction_market;
         pm.question = question;
         pm.start_at = start_at;
         pm.close_at = close_at;
-        pm.outcomes_count = outcomes_count;
+        pm.outcomes_count = 0;
+        pm.id = ctx.accounts.markets_count.value;
+        ctx.accounts.markets_count.value += 1;
         pm.oracle = *ctx.program_id;
-        
-        msg!(
-            "{} has created {}'s new prediction market: {}",
-            ctx.program_id,
-            ctx.accounts.user.key(),
-            pm.question
-        );
+
+        Ok(())
+    }
+
+    pub fn add_market_outcome(ctx: Context<AddMarketOutcome>, market_id: u64) -> Result<()> {
+        let outcome = &mut ctx.accounts.outcome;
+        outcome.market_id = market_id;
+        outcome.index = ctx.accounts.market.outcomes_count;
+        outcome.investments = 0;
+        ctx.accounts.market.outcomes_count += 1;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-#[instruction(question: String)]
+pub struct InitializeMarketsCount<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init_if_needed,
+        payer = user,
+        space = DISCRIMINATOR_SIZE + MarketCounter::INIT_SPACE,
+        seeds = [b"counter"],
+        bump
+    )]
+    pub markets_count: Account<'info, MarketCounter>,
+
+    pub system_program: Program<'info, System>,
+}
+
+
+#[derive(Accounts)]
 pub struct InitializePredictionMarket<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
 
     #[account(
+        init_if_needed,
+        payer = user,
+        space = DISCRIMINATOR_SIZE + MarketCounter::INIT_SPACE,
+        seeds = [b"counter"],
+        bump
+    )]
+    pub markets_count: Account<'info, MarketCounter>,
+
+    #[account(
         init,
         payer = user,
         space = DISCRIMINATOR_SIZE + PredictionMarket::INIT_SPACE,
-        seeds=[b"prediction_market", user.key().as_ref(), &question.as_bytes()],
+        seeds=[b"prediction_market", markets_count.value.to_le_bytes().as_ref()],
         bump,
     )]
     pub prediction_market: Account<'info, PredictionMarket>,
@@ -53,14 +88,54 @@ pub struct InitializePredictionMarket<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+#[instruction(market_id: u64)]
+pub struct AddMarketOutcome<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        space = DISCRIMINATOR_SIZE + Outcome::INIT_SPACE,
+        seeds = [b"outcome", market.id.to_le_bytes().as_ref(), market.outcomes_count.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub outcome: Account<'info, Outcome>,
+
+    #[account(
+        seeds = [b"prediction_market", market_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub market: Account<'info, PredictionMarket>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct MarketCounter {
+    pub value: u128,
+}
+
 #[account]
 #[derive(InitSpace)]
 pub struct PredictionMarket {
+    pub id: u128,
     #[max_len(256)]
     pub question: String,
-
     pub start_at: u64,
     pub close_at: u64,
     pub outcomes_count: u8,
     pub oracle: Pubkey,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Outcome {
+    pub market_id: u64,
+    #[max_len(32)]
+    pub title: String,
+    pub index: u8,
+    pub investments: u64,
 }
--- dapp/nostradamus/anchor/tests/nostradamus.test.ts
+++ dapp/nostradamus/anchor/tests/nostradamus.test.ts
@@ -3,36 +3,43 @@ const IDL = require('../target/idl/nostradamus.json')
 import { Nostradamus } from '../target/types/nostradamus'
 import { startAnchor, BankrunProvider } from 'anchor-bankrun'
 import { PublicKey } from '@solana/web3.js'
+import { ProgramTestContext } from 'solana-bankrun'
 const programAddress = new PublicKey('FqzkXZdwYjurnUKetJCAvaUw5WAqbwzU6gZEwydeEfqS')
 
 describe('nostradamus', () => {
-  it('Initialize nostradamus', async () => {
-    const ctx = await startAnchor('', [{ name: 'nostradamus', programId: programAddress }], []),
-      provider = new BankrunProvider(ctx)
+  let ctx: ProgramTestContext
+  let provider: BankrunProvider
+  let program: anchor.Program<Nostradamus>
 
-    const program = new anchor.Program<Nostradamus>(IDL, provider)
-    const question = 'Will this fuck run finally?'
+  beforeAll(async () => {
+    ctx = await startAnchor('', [{ name: 'nostradamus', programId: programAddress }], [])
+    provider = new BankrunProvider(ctx)
+    program = new anchor.Program<Nostradamus>(IDL, provider)
+    await program.methods.initializeMarketsCount().rpc()
+  })
+
+  it('Initialize nostradamus', async () => {
+    const question = 'Will this ?'
+    const [marketsCountAddress] = PublicKey.findProgramAddressSync([Buffer.from('counter')], programAddress)
+
+    const nextMarketId = await program.account.marketCounter.fetch(marketsCountAddress)
 
     await program.methods
       .initializePredictionMarket(
         question,
         new anchor.BN(Date.now() / 1e3),
         new anchor.BN(new Date(2026, 1, 1).getTime() / 1e3),
-        2,
       )
       .rpc()
-
     const [marketAddress] = PublicKey.findProgramAddressSync(
-      [Buffer.from('prediction_market'), provider.wallet.publicKey.toBuffer(), Buffer.from(question)],
+      [Buffer.from('prediction_market'), nextMarketId.value.toArrayLike(Buffer, 'le', 16)],
       programAddress,
-    );
-
-    const market = await program.account.predictionMarket.fetch(marketAddress);
-    
-    console.log(market);
-    expect(market.question).toEqual(question);
-    expect(market.oracle).toEqual(programAddress);
-    expect(market.outcomesCount).toEqual(2);
-    expect(market.startAt).not.toEqual(market.closeAt);
+    )
+    const market = await program.account.predictionMarket.fetch(marketAddress)
+    expect(market.id).toEqual(nextMarketId.value)
+    expect(market.question).toEqual(question)
+    expect(market.oracle).toEqual(programAddress)
+    expect(market.outcomesCount).toEqual(0)
+    expect(market.startAt).not.toEqual(market.closeAt)
   })
 })
